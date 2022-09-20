defdf997f9ee01862c81f1cd623b7132834357d7
Sakthi Subramaniam <sakthi_subramaniam@epam.com>
2022-09-20 10:00:11 +0100

    SwitchFeature

--- src/SwitchFeature.java
+++ src/SwitchFeature.java
@@ -9,26 +9,26 @@ public class SwitchFeature {
 
     private static String patternMatchingSwitch(Object value) {
         return switch (value) {
-            case Integer i -> "It is an Integer";
-            case String s -> "It is a String";
+            case Integer i -> i + " is an Integer";
+            case String s -> s + " is a String";
             default -> "Nat a valid type";
         };
     }
 
     private static String gaurdedPatternSwitch(Object value) {
         return switch (value) {
-            case Integer i -> "It is an Integer";
-            case String s -> "It is a String";
-            case Employee e && e.id() == 1L -> "It is the first employee";
+            case Integer i -> i + " is an Integer";
+            case String s -> s + " is a String";
+            case Employee e && e.id() == 1L -> e + " is the first employee";
             default -> "Nat a valid type";
         };
     }
 
     private static String nullCaseSwitch(Object value) {
         return switch (value) {
-            case Integer i -> "It is an Integer";
-            case String s -> "It is a String";
-            case Employee e && e.id() == 1L -> "It is the first employee";
+            case Integer i -> i + " is an Integer";
+            case String s -> s + " is a String";
+            case Employee e && e.id() == 1L -> e + " is the first employee";
             case null -> "It is a null value";
             default -> "Nat a valid type";
         };
